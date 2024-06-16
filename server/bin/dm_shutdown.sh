ff8b2659b94c63274a5db4c9a4767a4b6ba8f203
John Koepp <koeppj@koeppster.net>
2024-06-16 19:43:55 +0000

    Updated shurtdown script to account for external broker

--- server/bin/dm_shutdown.sh
+++ server/bin/dm_shutdown.sh
@@ -3,5 +3,8 @@
 echo "Gracefull shutdown..."
 $DOCUMENTUM/tomcat9.0.58/bin/stopMethodServer.sh
 $DOCUMENTUM/dba/dm_shutdown_$DM_DOCBASE_NAME
-$DOCUMENTUM/dba/dm_stop_Docbroker
+$DOCUMENTUM/dba/dm_stop_$DM_DOCBROKER_NAME
+if [[ -n "${DM_DOCBROKER_EXT_CONFIG}" ]]; then
+    $DOCUMENTUM/dba/dm_stop_$DM_DOCBROKER_EXT_NAME
+fi
 rm -r /opt/documentum/dba/running
